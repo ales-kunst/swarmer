8fe3222a3d6cc0e4befdf95aaacda66559db843a
Joe Specimen <joe.specimen@gmail.com>
2018-02-25 09:05:55 +0100

    Renamed packages.

---
+++ src/main/java/org/swarmer/SwarmerInputParams.java
@@ -0,0 +1,11 @@
+package org.swarmer;
+
+public class SwarmerInputParams {
+
+   public static String iniFilePath() {
+      return System.getProperty("swarm.starter.configuration.filepath", "D:\\programming\\java\\swarm-starter\\src\\test\\resources\\swarm_starter.ini");
+   }
+
+   private SwarmerInputParams() {
+   }
+}
--- src/main/java/org/swarmer/watcher/FolderChangesWatcher.java
+++ src/main/java/org/swarmer/watcher/FolderChangesWatcher.java
@@ -1,4 +1,4 @@
-package com.swarmer.watcher;
+package org.swarmer.watcher;
 
 import java.io.File;
 import java.io.IOException;
